8e6f773a115626e750492a7c9452feb895065a85
clara <10088152+claraj@users.noreply.github.com>
2022-02-17 15:01:42 -0600

    placeholder for favorites page

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ def home_page():
 
 @app.route('/get-cat')
 def get_cat():
-    category = request.args.get('category') #or 'space'  # set a default
-
+    # request.args is a dictionary-like object built from the data in the form in index.html
+    category = request.args.get('category') or 'space'  # set a default
+    
     cat_img_url = cat_img_api.get_cat_img(category)
     cat_fact = cat_fact_api.get_random_fact()
     cat_video = cat_video_api.cat_video(category)
@@ -28,6 +29,12 @@ def store_data():
     result = db.store_data('example')
     print(result)
     return redirect('/')
+
+
+@app.route('/favorites')
+def favorites_page():
+    # TODO get favorites from the database, include in response
+    return render_template('favorites.html')
 
 
 if __name__ == '__main__':
